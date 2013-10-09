3614a3a0efb96793f9fd06310637d3f4a5eea918
jauseg <jauseg@gmail.com>
2013-10-09 16:03:04 +0400

    added config

---
+++ src/ru/jauseg/arduinolights/cfg.java
@@ -0,0 +1,14 @@
+package ru.jauseg.arduinolights;
+
+import android.content.Context;
+
+public class cfg extends ApplicationPreferencesBase
+{
+	
+
+	public cfg(Context context, String name)
+	{
+		super(context, name);
+	}
+
+}
